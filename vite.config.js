1ee8666cfc40d3072d5c33a0bb07274a35db46d9
Developer <dev@example.com>
2026-06-02 22:30:43 +0800

    feat(sysstat): add /api proxy for backend

--- vite.config.js
+++ vite.config.js
@@ -229,6 +229,10 @@ export default defineConfig({
         changeOrigin: true,
         rewrite: (p) => p.replace(/^\/services\//, ''),
       },
+      '/api': {
+        target: 'http://localhost:5000',
+        changeOrigin: true,
+      },
     },
     fs: {
       allow: ['..'],
